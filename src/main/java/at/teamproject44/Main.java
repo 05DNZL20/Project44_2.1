2517cde24ea98cea511efe299520370de832abf0
Julia Sosnowski <j.sosnowski@gmx.at>
2022-01-14 19:32:48 +0100

    Julia: -readme as commentary in the main class
    readme is now on git

--- src/main/java/at/teamproject44/Main.java
+++ src/main/java/at/teamproject44/Main.java
@@ -10,16 +10,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-/*Hello, dear colleagues!
-We are in the process of making a battleship game.
-This is our project so far, it isn't complete, but you can start the game and place your ships.
-Have fun while trying out what we have so far!
-You can run the project with: View > Tool Windows > Gradle > Tasks > application > run
-
-Sincerely,
-the team from "Project 44"
-*/
-
 
 public class Main extends Application {
 
