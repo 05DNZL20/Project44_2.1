905c14f062b1a8f0e5831c2feda301c85b27920d
Merve Tuz <mervetuz11@gmail.com>
2022-01-23 22:31:25 +0100

    Kommentare einfügen -Main.java

--- src/main/java/at/teamproject44/Main.java
+++ src/main/java/at/teamproject44/Main.java
@@ -1,26 +1,43 @@
 package at.teamproject44;
 
+//Packages importieren
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Objects;
-
+// Main Methode startet Game und Ablauf
+//Klasse Application ist javafx.application
 public class Main extends Application {
 
-    @Override
+    //"MainMenu.fxml" wird aufgerufen.
+    @Override// → Methode muss überschrieben werden
+    //Methode heißt start und ist aus Klasse Application
+    //Wird verwendet um das primäre Anwendungsfenster zu erzeugen Ist abstrakt
     public void start(Stage primaryStage) throws Exception{
-            Parent root = FXMLLoader.load(this.getClass().getResource("MainMenu.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setTitle("Battleship");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        //Grafische Oberfläche MainMenu.fxml wird ins variable = root geladen
+        //Datentype Parent ist ein Referenz-Datentype (Datentypen von Objekten werden groß geschrieben)
+        Parent root = FXMLLoader.load(this.getClass().getResource("MainMenu.fxml")); // Spiel startet beim Main Menue
+        //Referenz Datentype = Scene
+        //Objekt von der Klasse Scene erstellen
+        //new Scene(root) = Konstruktor aufrufen von der Klasse Scene
+        Scene scene = new Scene(root);
+        //Eine Stage ist im Prinzip ein Fenster, das als „Bühne“ für unseren Inhalt dient
+        //Icon einfügen
+        primaryStage.getIcons().add(new Image("at/teamproject44/Images/Battleship_icon.jpeg"));
+        //Titel setzen
+        primaryStage.setTitle("Battleship");
+        //das Fenster soll eine feste größe haben
+        primaryStage.setResizable(false);
+        //Szene setzen
+        primaryStage.setScene(scene);
+        //Eine Stage wird mit Aufruf der Methode show()sichtbar
+        primaryStage.show();
     }
-
+    //Methode main
+    // launch(args) Method is inside the application class
     public static void main(String[] args) {
         launch(args);
     }
